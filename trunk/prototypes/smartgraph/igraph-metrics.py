f6924d010b47d267efd631c05f0711a6a306b1e8
chemaar <chema.ar@gmail.com>
2014-12-12 12:42:31 +0100

    Adding svm support

--- trunk/prototypes/smartgraph/igraph-metrics.py
+++ trunk/prototypes/smartgraph/igraph-metrics.py
@@ -21,6 +21,8 @@ from sklearn import datasets
 import numpy as np
 from sklearn import svm
 import matplotlib.pyplot as plt
+import random
+
 
 def todot(g,fout):
    g.write_dot(fout)
@@ -38,19 +40,71 @@ def extract_graph_metrics_from_file(fin):
    g = Graph.Read_GraphML(fin, False)
    return extract_graph_metrics(g)
   
+#From: http://igraph.wikidot.com/python-recipes
+
+def laplacian_centrality(graph, vs=None):
+   if vs is None:
+       vs = xrange(graph.vcount())
+   degrees = graph.degree(mode="all")
+   result = []
+   for v in vs:
+       neis = graph.neighbors(v, mode="all")
+       result.append(degrees[v]**2 + degrees[v] + 2 * sum(degrees[i] for i in neis))
+   return result
+
+
+def betweenness_centralization(G):
+    vnum = G.vcount()
+    if vnum < 3:
+        raise ValueError("graph must have at least three vertices")
+    denom = (vnum-1)*(vnum-2)
+ 
+    temparr = [2*i/denom for i in G.betweenness()]
+    max_temparr = max(temparr)
+    return sum(max_temparr-i for i in temparr)/(vnum-1)
+
+def create_svc_model(data, classification):
+    clf = svm.SVC()
+    clf.fit(data, classification) 
+    return clf
+
+def create_linear_svc_model(data, classification):
+    clf = svm.LinearSVC()
+    clf.fit(data, classification) 
+    return clf
+
+def classify_graph(clf, graph_instance):
+   dec = clf.decision_function([graph_instance])
+   return dec.shape[1]
+   
 if __name__ == "__main__":
-   # Load graph
-   g = extract_graph_metrics_from_file("graphs/primer.gml")
-   print (g)
-   # Extract characteristics
+   #Graphs
+   graphs = {}
+   # Load graph an external graph and extract characteristics
+   graphs["graphs/primer.gml"] = extract_graph_metrics_from_file("graphs/primer.gml")
+   #Create a random sample of graphs
+   for i in range (0,5):
+       graphs[i] = extract_graph_metrics(Graph.Erdos_Renyi(n=100, m=20))
    # Persist
    #foutput="graphs/primer.dot"
    #g.write_dot(foutput)
    #SVM: https://github.com/cjlin1/libsvm/tree/master/python
-   data = g.values()
-   target = g.keys()
-   print (data)
-   print(target)    
+   sample_data = []
+   sample_classification = []
+   ngraphs = len(graphs.keys())
+   for g in graphs.values():
+       sample_data.append(g.values())
+       sample_classification.append(random.randint(0,ngraphs))
+   #print (sample_data)    
+   #print (sample_classification)
+   #Learn: a list of lists X where each row is the metric of a graph, and a list of classifications (0,1,2)
+   #http://scikit-learn.org/stable/modules/svm.html#svm
+   clf = create_svc_model(sample_data, sample_classification) 
+   #Predict
+   graph_to_predict = [2, 20, 100, 87]
+   print classify_graph(clf,graph_to_predict)
+
+
 
     
     
