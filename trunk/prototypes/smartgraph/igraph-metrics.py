6e28c4b1ec7c1587096009d538b2defea70f4aab
chemaar <chema.ar@gmail.com>
2014-12-15 17:08:46 +0100

    Adding metrics

--- trunk/prototypes/smartgraph/igraph-metrics.py
+++ trunk/prototypes/smartgraph/igraph-metrics.py
@@ -31,12 +31,60 @@ def todot(g,fout):
 
 def extract_graph_metrics(g):
    graph_features={}
-   graph_features["vcount"] = g.vcount() 
-   graph_features["ecount"] = g.ecount()
-   graph_features["omega"] = g.omega()
-   graph_features["alpha"] = g.alpha()
-   #graph_features["laplacian_centrality"] = laplacian_centrality(g)
+   #Basic measures: http://igraph.org/python/doc/igraph.GraphBase-class.html
+   graph_features[g.vcount.__name__] = g.vcount() 
+   graph_features[g.ecount.__name__] = g.ecount()
+   graph_features[g.omega.__name__] = g.omega()
+   graph_features[g.alpha.__name__] = g.alpha()
+   graph_features[g.diameter.__name__] = g.diameter()
+   graph_features[g.average_path_length.__name__] = g.average_path_length()
+   graph_features[g.radius.__name__] = g.radius()
+       
+   #Structural properties
+   graph_features["max_"+g.degree.__name__] = max(g.degree())
+   graph_features["min_"+g.degree.__name__] = min(g.degree())
+   graph_features["mean_"+g.degree.__name__] = np.mean(g.degree())
+   graph_features["max_"+g.count_multiple.__name__] = max(g.count_multiple())
+   graph_features["min_"+g.count_multiple.__name__] = min(g.count_multiple())
+   graph_features["mean_"+g.count_multiple.__name__] = np.mean(g.count_multiple())
+   graph_features[g.has_multiple.__name__] = g.has_multiple()
+   graph_features[g.density.__name__] = g.density()
+   graph_features["max_"+g.diversity.__name__] = max(g.diversity())
+   graph_features["min_"+g.diversity.__name__] = min(g.diversity())
+   graph_features["mean_"+g.diversity.__name__] = np.mean(g.diversity())
+   graph_features["len_"+g.articulation_points.__name__] = len(g.articulation_points())
+   graph_features[g.assortativity_degree.__name__] = g.assortativity_degree()
+       
+
+   #Centrality
+   graph_features["max_laplacian_centrality"] = max(laplacian_centrality(g))
+   graph_features["min_laplacian_centrality"] = min(laplacian_centrality(g))
+   graph_features["mean_laplacian_centrality"] = np.mean(laplacian_centrality(g))
    graph_features["betweenness_centralization"] = betweenness_centralization(g)
+   betweeness_per_node=g.betweenness()
+   graph_features["max_"+g.edge_betweenness.__name__] = max(g.edge_betweenness())
+   graph_features["min_"+g.edge_betweenness.__name__] = min(g.edge_betweenness())
+   graph_features["mean_"+g.edge_betweenness.__name__] = np.mean(g.edge_betweenness())
+   graph_features["max_"+g.closeness.__name__] = max(g.closeness())
+   graph_features["min_"+g.closeness.__name__] = min(g.closeness())
+   graph_features["mean_"+g.closeness.__name__] = np.mean(g.closeness())
+   graph_features["max_in_"+g.closeness.__name__] = max(g.closeness(mode="in"))
+   graph_features["min_in_"+g.closeness.__name__] = min(g.closeness(mode="in"))
+   graph_features["mean_in_"+g.closeness.__name__] = np.mean(g.closeness(mode="in"))
+   graph_features["max_out_"+g.closeness.__name__] = max(g.closeness(mode="out"))
+   graph_features["min_out_"+g.closeness.__name__] = min(g.closeness(mode="out"))
+   graph_features["mean_out_"+g.closeness.__name__] = np.mean(g.closeness(mode="out"))
+   graph_features[g.canonical_permutation.__name__] = len(g.canonical_permutation())
+   graph_features[g.clique_number.__name__] = g.clique_number()
+   #graph_features[g.largest_cliques.__name__] = g.largest_cliques()
+
+   #Clustering
+   graph_features[g.count_automorphisms_vf2.__name__] = g.count_automorphisms_vf2()
+   graph_features["len_"+g.cut_vertices.__name__] = len(g.cut_vertices())
+   graph_features["len_"+g.knn.__name__] = len(g.knn())
+   graph_features["len_"+g.biconnected_components.__name__] = len(g.biconnected_components())
+
+   #clusters=g.clusters() #weak or strong
    return graph_features
 
 
@@ -93,15 +141,13 @@ if __name__ == "__main__":
    test_dir="./graphs/test/"
    # Load graph an external graph and extract characteristics
    train_graphs=load_graphs_from_dir(train_dir)
-   test_graphs=load_graphs_from_dir(test_dir)
-
-   print train_graphs
+   for fgraph in train_graphs.keys():
+       print fgraph
    # Persist
    #SVM: https://github.com/cjlin1/libsvm/tree/master/python
    max_classes = 10
    sample_data = []
    sample_classification = []
-   ngraphs = len(train_graphs.keys())
    for g in train_graphs.values():
        sample_data.append(g.values())
        sample_classification.append(random.randint(0,max_classes))
@@ -109,6 +155,7 @@ if __name__ == "__main__":
    #http://scikit-learn.org/stable/modules/svm.html#svm
    clf = create_svc_model(sample_data, sample_classification) 
    #Predict
+   test_graphs=load_graphs_from_dir(test_dir)
    for g in test_graphs.values():
         print classify_graph(clf,g.values())
 
